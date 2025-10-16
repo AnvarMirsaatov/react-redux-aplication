f27de90037d0a558dd4056e74f693215ab7db4e0
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-10-16 17:51:09 +0500

    validation added

--- src/ui/input.jsx
+++ src/ui/input.jsx
@@ -1,17 +1,20 @@
-import React from "react";
-
-const Input = ({ label, type = "text", state, setState }) => {
+const Input = ({ label, type = "text", state, setState, validate }) => {
   return (
-    <div className="form-floating">
+    <div className="form-floating mb-3">
       <input
         value={state}
         onChange={(e) => setState(e.target.value)}
         type={type}
-        className="form-control"
+        className={`form-control ${validate ? "is-invalid" : ""}`}
         id={label}
         placeholder={label}
       />
-      <label htmlFor="floatingInput">{label}</label>
+      {validate && (
+        <div className="invalid-feedback m-0 text-start p-0">
+          {validate}
+        </div>
+      )}
+      <label htmlFor={label}>{label}</label>
     </div>
   );
 };
