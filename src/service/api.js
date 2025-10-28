b3209212e6a432c7e0c89116ad0a1ffdf80b7e58
Anvar <158838160+AnvarMirsaatov@users.noreply.github.com>
2025-10-28 11:34:10 +0500

    api changed

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { getItem } from "../helpers/persistance-storage";
 
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "https://api.realworld.io/api ";
+// axios.defaults.baseURL = "http://localhost:3000/api";
 
 
 axios.interceptors.request.use((config) => {
